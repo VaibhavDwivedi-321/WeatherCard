b99262246246ea9b228bc942df90b385c7dc8492
vaibhav Dwivedi <vaibhavdwivedi3212gmail.com>
2023-02-06 01:48:14 +0530

    last

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const ejs = require('ejs');
 const https = require('https')
 const app = express();
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+
 app.use(bodyParser.urlencoded({extended:true}));
 require('dotenv').config();
 const apiKey = process.env.APP_ID;
